def8db974e4086b1abb9efa296d1cca329be93f9
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2017-11-22 20:19:09 -0200

    [API] Criação da camada de validação Base - Auth

--- api/src/validator/auth.js
+++ api/src/validator/auth.js
@@ -1,6 +1,6 @@
 import { default as _, validate } from 'lib/validator'
 
-const schema = _.object().keys({
+const schema = _.object({
   email: _.string().email().required(),
   password: _.string().required()
 })
